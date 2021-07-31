af13ce9c4c6e30a9a35c905d2130d03ac514bfb5
Lyubov <bunny207@rambler.ru>
2021-07-31 13:40:08 +0700

    Решение для задач 1-4

---
+++ task_1.py
@@ -0,0 +1,17 @@
+# Поработайте с переменными, создайте несколько, выведите на экран.
+# Запросите у пользователя некоторые числа и строки и сохраните в переменные, затем выведите на экран.
+
+number = 8
+print(number)
+
+name = ('Lyubov')
+print(name)
+
+name = input('What is your name? ' )
+print('Hi', name)
+
+city = input('Where are you from? ')
+print("I'm from", city, 'too')
+
+age = int(input('How old are you? '))
+print(age)
