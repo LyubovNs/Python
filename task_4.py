abb0ade1639e04398193ce1b3f2f1cf1a0df9b21
Lyubov <bunny207@rambler.ru>
2021-07-31 14:01:50 +0700

    Решение для 4 задачи

--- task_4.py
+++ task_4.py
@@ -2,10 +2,18 @@
 # Найдите самую большую цифру в числе. Для решения используйте цикл while и арифметические операции.
 
 number = int(input('Введите целое положительное число: '))
+maxnumb = number % 10
 if number < 0:
     print('Err!')
 else:
     print(number)
 
-while
-    p = number % 10
+while number >= 1:
+    number = number // 10
+    if number % 10 > maxnumb:
+        maxnumb = number % 10
+    if number > 9:
+        continue
+    else:
+        print("Максимальное цифра ", maxnumb)
+        break
