9add9756ca341d0e3dc9c84737b94c44f2da2aa9
Lyubov <bunny207@rambler.ru>
2021-08-10 00:06:13 +0700

    поправлено 1,4

--- task4_les3.py
+++ task4_les3.py
@@ -6,31 +6,24 @@
 # Первый — возведение в степень с помощью оператора **.
 # Второй — более сложная реализация без оператора **, предусматривающая использование цикла.
 
+# def my_func(x, y):
+#       if y != abs(y):
+#        result = x ** y
+#        return  result
+#       elif x != int(x):
+#        print('x должно быть целым положительным числом числом!')
+#       else:
+#        print('y должно быть отрицательным числом числом!')
+#
+# print(my_func(0.2, -3))
+
+import numbers
+
 def my_func(x, y):
-      if y != abs(y):
-       result = x ** y
-       return  result
-      elif x != int(x):
-       print('x должно быть целым положительным числом числом!')
-      else:
-       print('y должно быть отрицательным числом числом!')
+        if isinstance(x, numbers.Number):
+         result = x ** y
+         return result
+        else:
+         print('x > 0, y < 0')
 
-print(my_func(0.2, -3))
-
-
-
-
-def my_func(x, y):
-    result = x ** y
-    return result
-
-def check():
-    try:
-        x = int(x)
-        y != abs(y)
-    except (TypeError, ValueError) as err:
-        print('x должно быть целым положительным числом числом!')
-        print('y должно быть отрицательным числом числом!')
-
-print(my_func(1.2, -5, check))
-
+print(my_func(-12.4, -5))
