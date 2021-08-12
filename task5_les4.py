69d14ca7d44746a854b3cc422cc167e87ffc7c77
Lyubov <bunny207@rambler.ru>
2021-08-13 01:39:21 +0700

    Решение для задачи 5

--- task5_les4.py
+++ task5_les4.py
@@ -2,3 +2,12 @@
 # В список должны войти четные числа от 100 до 1000 (включая границы).
 # Необходимо получить результат вычисления произведения всех элементов списка.
 # Подсказка: использовать функцию reduce()
+
+from functools import reduce
+
+list = [numb for numb in range(100, 1000) if numb % 2 == 0]
+print(*list, sep = '\n')
+
+pr = reduce(lambda x,y: x * y, list)
+print(*pr)
+
