0a67c23d54fa0e773fca61033c53f8eb458fef47
Lyubov <bunny207@rambler.ru>
2021-08-14 23:57:54 +0700

    Решение для задачи 1

--- task1_les4.py
+++ task1_les4.py
@@ -3,3 +3,24 @@
 # (выработка в часах * ставка в час) + премия.
 # Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.
 
+from sys import argv
+
+# def check():
+#     work = float(input('Ведите выработку сотрудика в часах: '))
+#     cost = float(input('Ведите ставку сотрудика в час: '))
+#     happy = float(input('Ведите премию, начисленную сотрудику:'))
+#
+#     pay = (work * cost) + happy
+#     print(f'Размер заработной платы: {pay}')
+#
+# command = sys.argv[1]
+# if command == 'check':
+#     check()
+
+def check():
+    try:
+     time, rate, bon = map(float, argv[1:])
+     print(f'check - {time * rate + bon}')
+    except ValueError:
+        print('Введите 3 числа')
+
