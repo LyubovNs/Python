f341bd0b34ffabc03799598c53365e97b146e6a6
Lyubov <bunny207@rambler.ru>
2021-08-18 23:51:56 +0700

    Решение для задачи 4

--- task4_les5.py
+++ task4_les5.py
@@ -6,7 +6,8 @@
 # Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
 # При этом английские числительные должны заменяться на русские. Новый блок строк должен записываться в новый текстовый файл.
 
+num = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
 
-new_numbers = []
-
-with open('4.txt', 'r') as numbers:
+with open('text4.txt', 'w', encoding='utf-8') as new_num:
+    with open('text_4_txt', encoding='utf-8') as numbs:
+        new_num.writelines([line.replace(line.split()[0], num.get(line.split()[0])) for line in numbs])
